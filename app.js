4539143eae58d1d3ade0876da11272fd8e127b82
seyeon <tltptltptltp@gmail.com>
2020-02-13 22:28:13 +0900

    .painting

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas= document.getElementById("jsCanvas");
 const ctx= canvas.getContext("2d");
 
+canvas.width= 700;
+canvas.height= 700;
+
 ctx.strokeStyle="black";
 
 let painting = false;
@@ -12,9 +15,16 @@ function stopPainting() {
 function onMouseMove(e){
     const x= e.offsetX;
     const y= e.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
-function onMouseDown(e){
+function startPainting(e){
     painting= true;
 }
 
@@ -28,8 +38,8 @@ function onMouseLeave(e){
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
 
