4ace6be5be5f4c69271c2f94a82c5e2beb19867d
seyeon <tltptltptltp@gmail.com>
2020-02-14 16:32:48 +0900

    .change color

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const canvas= document.getElementById("jsCanvas");
 const ctx= canvas.getContext("2d");
+const colors= document.getElementsByClassName("jsColor");
 
-canvas.width= 700;
-canvas.height= 700;
+canvas.width= 800;
+canvas.height= 635;
 
 ctx.strokeStyle="black";
 
@@ -16,10 +17,14 @@ function onMouseMove(e){
     const x= e.offsetX;
     const y= e.offsetY;
     if(!painting){
+        //move mouse, not click
         ctx.beginPath();
+        //create path every time moving mouse
         ctx.moveTo(x,y);
     } else{
+        //click! painting-> true
         ctx.lineTo(x,y);
+        //create painting and stroking
         ctx.stroke();
     }
 }
@@ -36,6 +41,11 @@ function onMouseLeave(e){
     stopPainting();
 }
 
+function handleColorClick(e){
+   const color= e.target.style.backgroundcolor;
+   ctx.strokeStyle = color;
+}
+
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
@@ -43,3 +53,10 @@ if(canvas){
     canvas.addEventListener("mouseleave",stopPainting);
 }
 
+
+
+
+Array.from(colors).forEach(color=> 
+    color.addEventListener("click", handleColorClick)
+    //color ㄷㅏ른 이름이어도 괜찮음. array 안의 각 아이템 대표하는 역할
+    );
